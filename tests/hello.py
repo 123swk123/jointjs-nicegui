a0ec680d16a38d1081e3c6d3e5d7d7cea3d332eb
swk <swk@swkemb.com>
2024-11-10 04:01:33 +0900

    improved js loading and layout

--- tests/hello.py
+++ tests/hello.py
@@ -4,45 +4,51 @@ from nicegui import ui, events
 from nicegui.element import Element
 
 os.environ.update({"MATPLOTLIB": "false"})
-USE_JOINT_PLUS = os.environ.get("USE_JOINT_PLUS", "false").lower() == "true"
+USE_JOINT_PLUS = os.environ.get("USE_JOINT_PLUS", "true").lower() == "true"
 
-JOINT_JS_VUE_PATH = Path(__file__).parent.joinpath(
+PARENT_PATH=Path(__file__).parents[1]
+CWD_PATH=Path(__file__).parent
+
+JOINT_JS_VUE_PATH = CWD_PATH.joinpath(
     "joint1_vue.js" if USE_JOINT_PLUS else "joint2_vue.js"
 )
 JOINT_JS_MODULE_PATH = (
-    Path("L:/jointjs-vue-2/node_modules/@joint/plus")
+    PARENT_PATH.joinpath("node_modules/@joint/plus")
     if USE_JOINT_PLUS
-    else Path("L:/joint/packages/joint-core/dist")
+    else PARENT_PATH.joinpath("node_modules/@joint/core/dist")
 )
-JOINT_JS_PATH = (
-    JOINT_JS_MODULE_PATH.joinpath("joint-plus.js")
-    if USE_JOINT_PLUS
-    else JOINT_JS_MODULE_PATH.joinpath("joint.js")
-)
-JOINT_IMPORT_NAME = "joint-plus" if USE_JOINT_PLUS else "joint"
-
+JOINTJS_FILE_NAME = "joint-plus.js" if USE_JOINT_PLUS else "joint.js"
+JOINT_JS_PATH = JOINT_JS_MODULE_PATH.joinpath(JOINTJS_FILE_NAME)
+RESOURCE_PATH = CWD_PATH.joinpath("resources")
 
 class CJoint(
     Element,
     component=JOINT_JS_VUE_PATH,
-    dependencies=[JOINT_JS_PATH],
+    default_classes="nicegui-jointjs",
+    # default_style='position: fixed;',
+    # dependencies=[JOINT_JS_PATH],
 ):
     """Initialize CJoint."""
 
     def __init__(self):
         super(CJoint, self).__init__()
-        if USE_JOINT_PLUS:
-            self.add_resource(JOINT_JS_MODULE_PATH)
-            ui.add_head_html(
-                f'<link href="{self._props['resource_path']}/joint-plus.css" rel="stylesheet" type="text/css"/>'
-            )
+        self.add_resource(RESOURCE_PATH)
+        # if USE_JOINT_PLUS:
+        #     ui.add_head_html(
+        #         f'<link href="{self._props['resource_path']}/joint-plus.css" rel="stylesheet" type="text/css"/>'
+        #     )
         ui.add_body_html(
-            f'<script type="module">import * as _ from "{JOINT_IMPORT_NAME}";</script>'
+            f'<script src="{self._props['resource_path']}/{JOINTJS_FILE_NAME}"/>'
         )
+        print(self._style)
 
 
+ui.add_css(RESOURCE_PATH.joinpath("joint-plus.css"))
 ui.add_css(
     """
+.nicegui-jointjs {
+  position: fixed;
+}
  
 .canvas {
   width: 100%;
@@ -59,9 +65,9 @@ ui.add_css(
 objJoint = CJoint()
 
 ui.run(
-    host="127.0.0.1",
+    host="0.0.0.0",
     viewport="width=device-width, initial-scale=1, user-scalable=no",
-    dark=True,
+    dark=False,
     uvicorn_logging_level="info",
     show=False,
     reload=True,
